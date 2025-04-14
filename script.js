0e851390c6b9beda4bc84cdcd7e659ef17376190
sukhovdes <sukhov.des@gmail.com>
2025-04-14 20:09:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,5 @@ tg.expand();
 
 const user = tg.initDataUnsafe.user;
 
-document.getElementById("user-info").innerText =
-  `Ты зашел как ${user?.first_name || "гость"}`;
-
-function sendGreeting() {
-  tg.showAlert(`Приятно познакомиться, ${user?.first_name || "незнакомец"}!`);
-}
+const greetingEl = document.getElementById("greeting");
+greetingEl.innerText = `Привет, ${user?.first_name || "гость"}!`;
