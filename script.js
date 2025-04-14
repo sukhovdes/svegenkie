a88461efccd5fda652a768e064759e5138431d2a
sukhovdes <sukhov.des@gmail.com>
2025-04-14 20:11:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 const user = tg.initDataUnsafe.user;
+document.getElementById("greeting").innerText = `Привет, ${user?.first_name || "гость"}!`;
 
-const greetingEl = document.getElementById("greeting");
-greetingEl.innerText = `Привет, ${user?.first_name || "гость"}!`;
+function openScreen(id) {
+  document.querySelectorAll('.screen').forEach(el => el.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
+
+function goBack() {
+  document.querySelectorAll('.screen').forEach(el => el.classList.remove('active'));
+  document.getElementById("main").classList.add('active');
+}
